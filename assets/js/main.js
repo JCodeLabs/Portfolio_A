fd21582126ab5a7f38b3c6cc59f047c7589e69b6
JCodeCraftsman <jcyepesgarcia@gmail.com>
2024-04-16 14:53:43 -0500

    Agrega el header con funcionalidad de menu hamburguesa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,32 @@
 /* ==================== SHOW MENU ==================== */
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
 
+// show menu
+if(navToggle){
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
 
-/* ==================== REMOVE MENU MOBILE ==================== */
+// hidden menu
+if(navClose){
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+
+/* ========== remove menu mobile ========== */
+const navLink = document.querySelectorAll('.nav__link');
+
+navLink.forEach(element => {
+  element.addEventListener('click', () => {
+    const navMenu = document.getElementById('nav-menu');
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
+  });
+});
 
 
 /* ==================== SHADOW HEADER ==================== */
